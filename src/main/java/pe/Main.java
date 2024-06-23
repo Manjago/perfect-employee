c78f1cd551027b34f3a9d8124550a255e21abf81
Kirill Temnenkov <kirill@temnenkov.com>
2024-06-23 23:13:06 +0300

    final 1.0

--- src/main/java/pe/Main.java
+++ src/main/java/pe/Main.java
@@ -2,16 +2,28 @@ package pe;
 
 import java.awt.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, AWTException {
         final RandomSource randomSource = new RandomSource();
-        final Config config = new Config("src", ".java");
+        final String rootFileName = System.getProperty("user.home") + File.separator + "pe";
+        final Path rootPath = Paths.get(rootFileName);
+        if (!Files.exists(rootPath)) {
+            System.out.println("Where is my work data path " +rootFileName + "?");
+            System.exit(1);
+        }
+        if (!Files.isDirectory(rootPath)) {
+            System.out.println("My work data path " +rootFileName + " must be directory");
+            System.exit(2);
+        }
+        final Config config = new Config(rootFileName, ".java");
         final Typer typer = new Typer(randomSource, 50, 100);
 
         long totalLines = 0;
@@ -22,7 +34,7 @@ public class Main {
             final Lister lister = new Lister(config.getRoot(), config.getExt());
             final List<Path> paths = lister.allFiles();
             if (paths.isEmpty()) {
-                return;
+                System.exit(3);
             }
 
             final int index = randomSource.nextInt(paths.size());
@@ -34,11 +46,11 @@ public class Main {
                 while ((line = reader.readLine()) != null) {
                     totalCharacters += typer.typeLine(line);
                     ++totalLines;
-                    System.out.printf("\rPrinted %,18d lines, %,18d characters", totalLines, totalCharacters);
+                    System.out.printf("\rCurrent %,18d lines, %,18d characters", totalLines, totalCharacters);
                 }
             }
             typer.clean();
-            System.out.printf("\rTotal   %,18d lines, %,18d characters%n", totalLines, totalCharacters);
+            System.out.printf("\rPrinted %,18d lines, %,18d characters%n", totalLines, totalCharacters);
         }
     }
 }
