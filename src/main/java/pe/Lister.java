df339a2318b5e14d4150701c7b562fdf6ed1d0f3
Kirill Temnenkov <kirill@temnenkov.com>
2024-07-03 22:18:20 +0300

    unused imports
    Took 18 minutes

--- src/main/java/pe/Lister.java
+++ src/main/java/pe/Lister.java
@@ -5,7 +5,6 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Lister {
